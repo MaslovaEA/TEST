84d711523cfbbf39a594704451677fd44fd65ba2
grupa203 <grupa203git@mail.ru>
2023-10-31 22:13:10 +0300

    wpf

---
+++ WpfApp4/WpfApp4/Pages/PageCreateAcc.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.ApplicationData;
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageCreateAcc.xaml
+    /// </summary>
+    public partial class PageCreateAcc : Page
+    {
+        public PageCreateAcc()
+        {
+            InitializeComponent();
+            cmbRole.ItemsSource = ProbaEntities.GetContext().Role.ToList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.GoBack();
+        }
+
+        private void psbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (psbPassword.Password != txbPass.Text)
+            {
+                btnCreate.IsEnabled = false;
+                psbPassword.Background = Brushes.LightCoral;
+                psbPassword.BorderBrush = Brushes.Red;                
+            }
+            else
+            {
+                btnCreate.IsEnabled = true;
+                psbPassword.Background = Brushes.LightGreen;
+                psbPassword.BorderBrush = Brushes.Green;
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string _NameRole;
+            int _idRole;
+
+            if (ProbaEntities.GetContext().Person.Count(x => x.login == txbLog.Text) > 0)
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            
+            try
+            {
+                _NameRole = cmbRole.Text;
+                var RoleObj = ProbaEntities.GetContext().Role.FirstOrDefault(x => x.NameRole == _NameRole);
+                _idRole = RoleObj.id;
+                Person userObj = new Person()
+                {
+                    login = txbLog.Text,
+                    name = txbName.Text,
+                    password = txbPass.Text,
+                    idrole = _idRole
+                };
+                ProbaEntities.GetContext().Person.Add(userObj);
+                ProbaEntities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно добавлены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                //txbLog.Clear();
+                //txbName.Clear();
+                //txbPass.Clear();
+                //psbPassword.Clear();
+                AppFrame.MainFrame.Navigate(new PageLogin());
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при добавлении данных!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+    }
+}
---
+++ WpfApp4/WpfApp4/Pages/PageLogin.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.ApplicationData;
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToComeIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = ProbaEntities.GetContext().Person.FirstOrDefault(x => x.login == txbLogin.Text && x.password == psbPassword.Password);
+                if (userObj == null) 
+                {
+                    MessageBox.Show("Пользователь с таким логином и паролем не найден!","Ошибка авторизации",MessageBoxButton.OK,MessageBoxImage.Error);
+                }
+                else
+                {
+                    AppFrame.MainFrame.Navigate(new MainMenu());
+                    switch (userObj.idrole)
+                    {
+                        case 1:
+                            Application.Current.MainWindow.Title = userObj.name + " (Менеджер)";
+                            //MessageBox.Show("Здравствуйте, менеджер " + userObj.name +"!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        case 2:
+                            Application.Current.MainWindow.Title = userObj.name + " (Продавец)";
+                            //MessageBox.Show("Здравствуйте, продавец " + userObj.name + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                    }
+                    
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка:" + Ex.Message.ToString(), "Критическая работа приложения", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void btnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new PageCreateAcc());
+        }
+    }
+}
